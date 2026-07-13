1e8075e392d43f946c4a2ddaae69235ee25ae9ec
Mohammad Hammad <hammad.scripted@gmail.com>
2026-07-13 22:09:42 +0530

    streams and box

--- src/main/java/api/stream/StreamEx.java
+++ src/main/java/api/stream/StreamEx.java
@@ -1,7 +1,10 @@
 package api.stream;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamEx {
@@ -13,6 +16,37 @@ public class StreamEx {
         Stream<Integer> stream1 = list.stream();
         System.out.println(stream.filter(n -> n % 2 == 0));
         System.out.println(stream1.map(n -> n * 2));
+
+
+//? before streams
+        List<String> names = Arrays.asList("Alice", "Bob", "Alex", "Charlie");
+        List<String> result = names.stream().filter(name -> name.startsWith("A")).map(String::toUpperCase).collect(Collectors.toList());
+
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(23);
+        nums.add(12);
+        nums.add(9);
+        nums.add(43);
+        nums.add(8);
+        nums.add(1);
+        nums.add(0);
+        nums.add(98);
+
+        Stream<Integer> s1 = nums.stream();
+//        Predicate<Integer> p = new Predicate<Integer>() {
+//            @Override
+//            public boolean test(Integer integer) {
+//                return integer % 2 == 0;
+//            }
+//        };
+
+        Predicate<Integer> p = (integer) -> {
+            return integer % 2 == 0;
+        };
+
+        List<Integer> result1 = s1.filter(p).collect(Collectors.toList());
+        System.out.println(result1);
 
 
     }
---
+++ src/main/java/com/collection/Box.java
@@ -0,0 +1,24 @@
+package com.collection;
+
+public class Box<T> {
+    private T value;
+
+    Box(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "value=" + value +
+                '}';
+    }
+}
